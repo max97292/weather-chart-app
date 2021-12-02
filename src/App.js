97d8cd1cb34e2da1f04420d62f66293bf156d683
Maksym Diachenko <max97292@gmail.com>
2021-12-02 14:08:54 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+// openweathermap.org
+// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
+// api key: bb96c7f9ac6f57dc00333727c5407547
+
+// google maps api
+// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* form goes here */}
+      {/* chart goes here */}
     </div>
   );
 }
-
-export default App;
