981a0b0f731327a9f4a33bbd8c796e697390f7e4
Maksym Diachenko <max97292@gmail.com>
2021-12-02 16:26:58 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GeoForm from './components/GeoForm';
+import WeatherChart from './components/WeatherChart';
 import './App.css';
 
-// openweathermap.org
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,minutely&units=imperial&appid=bb96c7f9ac6f57dc00333727c5407547
-// api key: bb96c7f9ac6f57dc00333727c5407547
-
-// google maps api
-// api key: AIzaSyDp4os33WF6-4d-xFVyL0HsUUHN7dOml_w
-
 export default function App() {
+  const [latLng, setLatLng] = useState(null);
+
   return (
     <div className="app">
       {/* form goes here */}
+      <GeoForm setLatLng={setLatLng} />
+
       {/* chart goes here */}
+      {latLng && <WeatherChart latLng={latLng} />}
     </div>
   );
 }
